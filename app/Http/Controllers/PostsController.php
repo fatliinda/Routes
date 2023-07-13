b1b86ad67fa46c072888dc5953a1d55bdd5d30e3
Fatlinda <fatlindabrahaj@gmail.com>
2023-07-13 19:44:23 +0200

    db

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostsController extends Controller
 {
      public function index(){
             return view('posts/index');
-
-
+            $post=DB::select('select*from posts');
+            dd($post);
 
 
      }
